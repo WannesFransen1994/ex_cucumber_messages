fcd459bb4ac2dcb74f9d7c56392b03a39c33d1ff
Wannes Fransen <wannes.fransen@ucll.be>
2020-07-14 12:49:58 +0200

    bugfix, possibly...

--- lib/cucumber_messages/writer.ex
+++ lib/cucumber_messages/writer.ex
@@ -1,8 +1,41 @@
 defmodule ExCucumberMessages.Writer do
-  defp unstruct(%{__struct__: _} = map), do: map |> Map.from_struct() |> unstruct()
-  defp unstruct(%{} = map), do: Enum.map(map, fn {k, v} -> {k, unstruct(v)} end) |> Enum.into(%{})
-  defp unstruct(list) when is_list(list), do: Enum.map(list, fn el -> unstruct(el) end)
-  defp unstruct(just_a_value), do: just_a_value
+  defp unstruct(%{__struct__: _} = map) do
+    map
+    |> Map.from_struct()
+    |> unstruct()
+    |> case do
+      {:ok, v} -> v
+      {:replace, k, v} -> %{k => unstruct(v)}
+      data -> data
+    end
+  end
+
+  defp unstruct(%{} = map) do
+    Enum.map(map, fn {k, v} ->
+      new_v =
+        case unstruct(v) do
+          {:ok, v} -> v
+          {:replace, k, v} -> %{k => unstruct(v)}
+          data -> data
+        end
+
+      {k, new_v}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp unstruct(list) when is_list(list) do
+    Enum.map(list, fn el ->
+      case unstruct(el) do
+        {:ok, v} -> v
+        {:replace, k, v} -> %{k => unstruct(v)}
+        data -> data
+      end
+    end)
+  end
+
+  defp unstruct({key, value}), do: {:replace, key, value}
+  defp unstruct(just_a_value), do: {:ok, just_a_value}
 
   def envelopes_to_ndjson!(list_of_envelopes) when is_list(list_of_envelopes) do
     list_of_envelopes
