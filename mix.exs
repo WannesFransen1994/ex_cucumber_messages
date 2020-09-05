2bd1995f0b271c799c235539b3a9544b5a74aba6
Wannes Fransen <wannes.fransen@ucll.be>
2020-09-05 12:51:50 +0200

    project info

--- mix.exs
+++ mix.exs
@@ -2,10 +2,17 @@ defmodule ExCucumberMessages.MixProject do
   @moduledoc false
   use Mix.Project
 
+  @vsn "0.1.0"
+  @github "https://github.com/WannesFransen1994/ex_cucumber_messages"
+  @name "ExCucumberMessages"
+
   def project do
     [
       app: :ex_cucumber_messages,
-      version: "0.1.0",
+      version: @vsn,
+      name: @name,
+      description: description(),
+      package: package(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,7 +30,20 @@ defmodule ExCucumberMessages.MixProject do
   defp deps do
     [
       {:protox, "~> 0.25.0"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir implementation of the cucumber messages protobuf schema"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      source_url: @github,
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 end
